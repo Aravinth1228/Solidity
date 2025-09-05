9e214dbd1a5a11e308eeafc19026a3ae015cfd58
Aravinthan Selvam <133775879+Aravinth1228@users.noreply.github.com>
2025-09-05 19:15:29 +0530

    Create Calucater.sol

---
+++ Calucater.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Calculator {
+    function add(uint256 a, uint256 b) external pure returns (uint256) {
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) external pure returns (uint256) {
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) external pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) external pure returns (uint256) {
+        require(b > 0, "Divide by zero");
+        return a / b;
+    }
+}
